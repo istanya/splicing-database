1d4253095c6fab0e7dde25719d60b84d5484958d
Tatiana Isaeva <tanisa@MacBook-Pro-Tatiana.local>
2024-09-18 00:08:22 +0400

    init project

---
+++ src/components/layouts/BaseHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <el-menu default-active="1" class="el-menu-demo" mode="horizontal">
+    <el-menu-item index="1">About</el-menu-item>
+    <el-menu-item index="2">How to use</el-menu-item>
+    <el-menu-item index="3">Samples</el-menu-item>
+  </el-menu>
+</template>
---
+++ src/components/layouts/BaseSide.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-menu
+    class="el-menu-vertical-demo"
+    :collapse="false"
+  >
+  <div>
+    <p>Enter gene id</p>
+  </div>
+  <div>
+  <el-input 
+    v-model="input1"
+    class="m-2" 
+    style="width: 90%"
+    placeholder="Please input"
+  >
+    <template #append>
+          <el-button :icon="Search" />
+    </template>
+  </el-input>
+  </div>
+  <div>
+  <el-button type="primary" plain>show genome browser</el-button>
+  </div>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import { Search } from '@element-plus/icons-vue'
+
+  const input1 = ref('')
+</script>
+
+<style>
+.input-with-select .el-input-group__prepend {
+  background-color: var(--el-fill-color-blank);
+}
+</style>
